dd405e97505f38e1c0183dac1cb7916cb760b259
Justin <justinszabo99@gmail.com>
2019-01-23 15:04:34 +0000

    Added functionality and comments, user can type in their own year

--- src/leapyear/LeapYear.java
+++ src/leapyear/LeapYear.java
@@ -3,8 +3,7 @@
  */
 package leapyear;
 
-import java.util.Arrays;
-
+import java.util.Scanner;
 /**
  * @author User
  *
@@ -12,33 +11,50 @@ import java.util.Arrays;
 public class LeapYear {
 	
 	public static void main(String[] args) {
-		LeapYear.calcYear(2019);
+		
+		// inits
+		
+		Scanner sc = new Scanner(System.in);
+		int year;
+		
+		// prompt for year
+		System.out.println("Enter your year: ");
+		year = sc.nextInt();
+		sc.close();
+		
+		// Calls function to calculate leap year
+		
+		LeapYear.calcYear(year);
 	
 	}
 
 	public static void calcYear(int year) {
+		
+		// inits
+		
 		int i = 0;
 		int y = 0;
 		y = year ;
 		
-		while (i != 20) 
+		// Main body
+		// Opens while loop and then checks
+		// if a leap year and then prints if true
+		while (i != 20)
 		{
 			if((y % 400 == 0) || ((y % 4 == 0) && (y % 100 != 0)))
 			{
 				System.out.println(y);
+				// Increments year and counter
 				i++;
 				y++;
 			}	
-			else 
+			else // If not a leap year then just increment
 			{
 				i++;
 				y++;
 			} 
-		}
-		
-	}
-	
-
+		}	
+	}	
 }
 	
 
