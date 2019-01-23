109c9a3768e5f0faa033dcead28782ded07e479d
Justin <justinszabo99@gmail.com>
2019-01-23 14:38:41 +0000

    It actually works

--- src/leapyear/LeapYear.java
+++ src/leapyear/LeapYear.java
@@ -15,22 +15,15 @@ public class LeapYear {
 		LeapYear.calcYear(2019);
 	
 	}
-	
-	private static void printArray(int[] anArray) {
-		
-		System.out.println(anArray.toString());
-		
-	}	
+
 	public static void calcYear(int year) {
-		int[] yArray;
-		yArray = new int[20];
 		int i = 0;
 		int y = 0;
 		y = year ;
 		
-		while (i <= 19) 
+		while (i != 20) 
 		{
-			if((y % 4 == 0) && (y % 100 != 0)) 
+			if((y % 400 == 0) || ((y % 4 == 0) && (y % 100 != 0)))
 			{
 				System.out.println(y);
 				i++;
