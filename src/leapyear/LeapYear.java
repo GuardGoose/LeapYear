eeb5c91e5f88169c74c2e4a518b6e2ad1b8d0835
Justin <justinszabo99@gmail.com>
2019-01-23 13:06:17 +0000

    This is risky because it might not work

--- src/leapyear/LeapYear.java
+++ src/leapyear/LeapYear.java
@@ -3,6 +3,8 @@
  */
 package leapyear;
 
+import java.util.Arrays;
+
 /**
  * @author User
  *
@@ -10,7 +12,22 @@ package leapyear;
 public class LeapYear {
 	
 	public static void main(String[] args) {
-		int[] anArray;
+		int[] yArray;
+		yArray = new int[20];
+		int year = 2019;
+		int i = 0;
+		
+		if (i < 20) {
+			if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+				yArray[i] = year;
+				System.out.println(Arrays.toString(yArray));
+				i++;
+			} else {
+				i++;
+			}
+		} else {
+			System.out.println(Arrays.toString(yArray));
+		}
 		
 	
 	}
